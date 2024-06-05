32652ff85dc588fd60397227850456b66551a816
Muhammad Fouzan Akhter <161457773+fouzan-akhter@users.noreply.github.com>
2024-06-06 02:14:52 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+from processor import VectorStore_Extractor
+from processor import Compute_Response
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+st.set_page_config(page_title="Web-Chat App", page_icon="custom_icon.png")
+st.title("Web-Chat App")
+website_url = st.text_input("Please enter a valid URL you wish to converse with:")
+if website_url is None or website_url == "":
+    st.info("Please enter a valid website URL to start the chat.")
+else:
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hi there, I am the Web-Chat Bot. How can I help you today?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = VectorStore_Extractor(website_url)
+    user_query = st.chat_input("Type your message here...")
+    if user_query:
+        response = Compute_Response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
+footer = """
+    <style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+        color: black;
+        text-align: center;
+        padding: 10px;
+        font-size: 14px;
+    }
+    </style>
+    <div class="footer">
+        <p>Check out my GitHub portfolio: <a href="https://github.com/your-username" target="_blank">your-username</a></p>
+    </div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
---
+++ processor.py
@@ -0,0 +1,47 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+import streamlit as st
+
+
+def VectorStore_Extractor(url):
+    loader = WebBaseLoader(url)
+    document = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    return vector_store
+
+def ContextRetriever_Chain(vector_store):
+    llm = ChatOpenAI()
+    retriever = vector_store.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    return retriever_chain
+
+def RAG_Chain(retriever_chain): 
+    llm = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def Compute_Response(user_input):
+    retriever_chain = ContextRetriever_Chain(st.session_state.vector_store)
+    conversation_rag_chain = RAG_Chain(retriever_chain)
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    return response['answer']
